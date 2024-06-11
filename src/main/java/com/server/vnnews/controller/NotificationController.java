9216edf34956d9273a6d00d00a4ba18ff9d9edf3
Phan Hà <167295017+pnlamha8@users.noreply.github.com>
2024-06-11 22:53:35 +0000

    fix api

--- src/main/java/com/server/vnnews/controller/NotificationController.java
+++ src/main/java/com/server/vnnews/controller/NotificationController.java
@@ -3,6 +3,7 @@ package com.server.vnnews.controller;
 import com.server.vnnews.dto.NotificationDTO;
 import com.server.vnnews.entity.Notification;
 import com.server.vnnews.service.NotificationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import java.util.List;
 
 @RestController
 public class NotificationController {
-
+    @Autowired
     private NotificationService notificationService;
     // notification by user i
     @GetMapping("/notification/{userId}")
