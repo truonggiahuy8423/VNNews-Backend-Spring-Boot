16e5af88e015dde1cf882189542b5f2e16a79025
namnguyen147 <namnguyen147159@gmail.com>
2024-06-15 19:48:24 +0700

    Complete Favorite Article Backend

--- src/main/java/com/server/vnnews/entity/composite/FavoriteId.java
+++ src/main/java/com/server/vnnews/entity/composite/FavoriteId.java
@@ -1,6 +1,7 @@
 package com.server.vnnews.entity.composite;
 import jakarta.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
 @Embeddable
 public class FavoriteId implements Serializable {
@@ -11,5 +12,40 @@ public class FavoriteId implements Serializable {
     @Column(name = "article_id")
     private Long articleId;
 
-    // Constructors, getters, and setters
+    public FavoriteId() {}
+
+    public FavoriteId(Long userId, Long articleId) {
+        this.userId = userId;
+        this.articleId = articleId;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public Long getArticleId() {
+        return articleId;
+    }
+
+    public void setArticleId(Long articleId) {
+        this.articleId = articleId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FavoriteId that = (FavoriteId) o;
+        return Objects.equals(userId, that.userId) &&
+                Objects.equals(articleId, that.articleId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, articleId);
+    }
 }
