a09baeb15439216f919d9bcbea1d48646b48b8d5
namnguyen147 <namnguyen147159@gmail.com>
2024-06-15 20:17:26 +0700

    Complete See Later Article Backend

--- src/main/java/com/server/vnnews/repository/SeeLaterRepository.java
+++ src/main/java/com/server/vnnews/repository/SeeLaterRepository.java
@@ -1,8 +1,15 @@
 package com.server.vnnews.repository;
 
+import com.server.vnnews.entity.Article;
 import com.server.vnnews.entity.SeeLater;
 import com.server.vnnews.entity.composite.SeeLaterId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface SeeLaterRepository extends JpaRepository<SeeLater, SeeLaterId> {
+
+    @Query("SELECT s.article FROM SeeLater s")
+    List<Article> findAllSeeLaterArticles();
 }
